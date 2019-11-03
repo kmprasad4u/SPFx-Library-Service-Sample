f35d433c9bb11d3d00606dd7ace7e29d1170838a
Prasad Kumaraswamy Mallika <Prasad_Mallika@ad.infosys.com>
2019-11-03 08:04:36 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## SPFx-Library-Service-Sample
+[How to set context for pnpjs in Service ?](https://github.com/pnp/pnpjs/issues/869)
+
+### Building the code
+* Change Site Url in src\SPFx-WebPart\Config\serve.json
+* In Termainal 1
+    * cd src\SPFx-Library
+    * npm install
+    * gulp serve
+* In Terminal 2
+    * cd src\SPFx-WebPart
+    * npm install
+    * gulp serve
+* Edit the page (NOT online workbench) and add Sample WebPart
---
+++ src/SPFx-Library/src/libraries/sampleLibrary/SampleLibraryLibrary.ts
@@ -0,0 +1,35 @@
+import { ServiceKey, ServiceScope } from "@microsoft/sp-core-library";
+import { PageContext } from "@microsoft/sp-page-context";
+import { sp } from "@pnp/sp";
+
+export interface ISampleService {
+  getLists() : Promise<any[]>;
+}
+
+export class SampleService implements ISampleService {
+
+  public static readonly serviceKey : ServiceKey<ISampleService> = ServiceKey.create<ISampleService>('SPFx:SampleService', SampleService);    
+  
+  constructor(serviceScope : ServiceScope) {
+    serviceScope.whenFinished(() => {  
+      const pageContext = serviceScope.consume(PageContext.serviceKey);  
+      
+      // Cannot use this, as spfxContext expects this.context and not this.context.pageContext
+      // Is there a way to get this.context using serviceScope ?
+      //sp.setup({
+        //spfxContext : pageContext  
+      //})
+
+      // Workaround works, but not sure if its the best approach
+      // When this is not set, we get error in console
+      // sp.setup({
+      //   sp : {
+      //     baseUrl : pageContext.web.absoluteUrl
+      //   }
+      // });
+    });
+  }
+  public getLists(): Promise<any[]> {
+    return sp.web.lists.get();
+  }
+}
